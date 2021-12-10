b7fbd7f0feda0bce78f670e8cd7c6cd220e8dd87
Jack Reilly <jack@vianova.io>
2021-12-10 06:51:20 +0100

    d11

--- d11a.py
+++ d11a.py
@@ -1,25 +1,22 @@
 import functools
 from pathlib import Path
+from deepnone import dn
 
 flipped = {"{": "}", "[": "]", "(": ")", "<": ">"}
-flipped2 = {v: k for k, v in flipped.items()}
 
 
-def score(line: str, stack: list = None) -> int:
-    if stack is None:
-        stack = []
+def score(line: str, stack: list = []) -> int:
     if not line:
         return functools.reduce(lambda x, y: x * 5 + " )]}>".index(y), stack[::-1], 0)
     head, *tail = line
-    if head in flipped:
-        stack.append(flipped[head])
-        return score(tail, stack)
-    if not stack:
-        return 0
-    if head == stack[-1]:
-        stack.pop()
-        return score(tail, stack)
-    return 0
+    return (
+        score(tail, [*stack, flipped[head]])
+        if head in flipped
+        else score(tail, stack[:-1])
+        if dn(stack)[-1] == head
+        else 0
+    )
+
 
 scores = sorted(filter(bool, map(score, Path("11.txt").read_text().splitlines())))
 print(scores[len(scores) // 2])
